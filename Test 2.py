934f754fb71b0eab54afa2b06b4efa603c4d5271
PutteFerdi <90191269+PutteFerdi@users.noreply.github.com>
2021-09-13 13:12:53 +0200

    ✨ Räkna

--- Test 2.py
+++ Test 2.py
@@ -1,6 +1,31 @@
-name = input('Vad heter du? ')
+# name = input('Vad heter du? ')
 
-print(f'Hej på dig {name}')
+# print(f'Hej på dig {name}')
 
-age = input('Hur gammal är du? ')
-print (f'Oj så gammal du är, hela {age} år.')
+# age = input('Hur gammal är du? ')
+
+# print (f'Oj så gammal du är, hela {age} år.')
+
+
+
+tal1 = 2  
+
+tal2 = float(input("Skriv et tal: "))
+
+svar1 = tal1 + tal2
+print(f"summan av {tal1} + {tal2} = {svar1}")
+svar1 = tal1 - tal2
+print(f"summan av {tal1} - {tal2} = {svar1}")
+svar1 = tal1 * tal2
+print(f"summan av {tal1} * {tal2} = {svar1}")
+svar1 = tal1 / tal2
+print(f"summan av {tal1} / {tal2} = {svar1}")
+
+
+# print (f"summan blir {svar1}")
+
+# print (f"summan blir {svar1}")
+
+# print (f"summan blir {svar1}")
+
+# print (f"summan blir {svar1}")
