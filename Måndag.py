bdf7528a3dbadec059e3b884e42e713bcdc1309b
PutteFerdi <90191269+PutteFerdi@users.noreply.github.com>
2021-10-04 13:21:19 +0200

    🎉 måndag

---
+++ Måndag.py
@@ -0,0 +1,18 @@
+# age = input("Mata in din ålder: ")
+
+# if age > "18":
+#     print("Stick och brinn")
+#     exit()
+
+# else:
+#     print ("Sätt dig i skolbänken")
+
+user_input = input("Skriv ditt lösenord: ")
+super_secret_password = "1337"
+
+if user_input != super_secret_password:
+    print("stick")
+    exit()
+
+else:
+    print("Du har tillåtelse att köra denna BMW E46 330i")
